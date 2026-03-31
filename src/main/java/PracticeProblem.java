7f700a0bedc4578f111d0a44a6750c37d65d0b27
Nithin Alagar <343954178@tdsb.ca>
2026-03-31 14:41:04 +0000

    Nithin.A

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,7 +1,18 @@
+/**
+
+        * File: Lesson 3.5 - Problem solving and good programing style
+
+        * Author: Nithin.A
+
+        * Date Created: March 30, 2026
+
+        * Date Last Modified: March 31, 2026
+
+        */
 public class PracticeProblem {
 
 	public static void main(String args[]) {
-
+		System.out.println("John".indexOf(" "));
 	}
 
 	public static void q1() {
@@ -23,5 +34,39 @@ public class PracticeProblem {
 	public static void q5() {
 		//Write question 5 code here
 	}
+	public static String getFirstName(String name){
+		name = name.trim();
+		int index = name.indexOf(" ");
+		return name.substring(0, index);
+	}
+	public static String getLastName(String name){
+		name = name.trim();
+		int index = name.lastIndexOf(" ");
+		return name.substring(index+1);
+	}
+	public static boolean isValidName(String name){
+		name = name.trim();
+		int indexFirst = name.indexOf(" ");
+		int indexLast = name.lastIndexOf(" ");
+
+		if (!name.contains(" ") || (name.indexOf(" ") != name.lastIndexOf(" "))){
+			return false;
+		}
+		else if ((!(2 <= name.substring(0, indexFirst).length() && 2<=name.substring(indexLast+1).length()))){
+			return false;
+		}
+		else {
+			return true;
+
+		}
+
+		// if (2 >= name.substring(0, indexFirst).length() && name.substring(indexLast+1)){
+		// 	return false;
+		// }
+		// else {
+		// 	return name.indexOf(" ") == name.lastIndexOf(" ");
+		// }
+	}
+	
 
 }
